9833f55527a125388e76f955dbaf288fa256a6dc
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2024-06-21 16:57:10 -0300

    feat: add question 10 answers

---
+++ Question10/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Question10;
+
+internal static class Program
+{
+    public static void Main(string[] _)
+    {
+        Console.WriteLine(Calculate(1, 2));
+        Console.WriteLine(Calculate(8, 2));
+        Console.WriteLine(Calculate(24, 5));
+    }
+
+    private static (int sum, int product) Calculate(int value1, int value2)
+    {
+        var sum = value1 + value2;
+        var product = value1 * value2;
+        return (sum, product);
+    }
+}
---
+++ Question10/README.md
@@ -0,0 +1,3 @@
+Escreva uma função do tipo inteiro que recebe como parâmetros dois números e cujo retorno seja a soma desses dois valores.
+
+Em seguida, faça uma alteração nessa função que permita que, de alguma forma, ela forneça também o resultado da multiplicação dos dois valores, além do valor da soma dos dois valores que ela já retornava. 
