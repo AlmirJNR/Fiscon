868755bd5bbe9df88d63889c69600e7e51fbca66
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2024-06-21 17:24:34 -0300

    feat: add question 15 answers

---
+++ Question15/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Question15;
+
+public static class Program
+{
+    private const int Ground = 100;
+    private const int Ceiling = 200;
+
+    public static void Main(string[] _)
+    {
+        var oddNumbersSum = 0;
+        for (var i = Ground; i <= Ceiling; i++)
+            if (Mod(i, 2) != 0)
+                oddNumbersSum += i;
+
+        Console.WriteLine($"Sum of odd values between {Ground} and {Ceiling} is {oddNumbersSum}");
+    }
+
+    /// <returns>O resto da divisão entre dois números</returns>
+    private static int Mod(int a, int b) => a % b;
+}
---
+++ Question15/README.md
@@ -0,0 +1,5 @@
+Faça um programa que calcule a soma dos números ímpares entre 100 e 200.
+
+Obs.: Supor a existência do comando “mod”, que retorna o resto da divisão entre dois números.
+
+IMPORTANTE: Utilize o algoritmo mais veloz que puder definir.
